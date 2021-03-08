cd8d9f7ae8e65622d6f0457576e82bf5d5dfdc30
kingurru <kingnikoriginal@gmail.com>
2021-03-08 22:23:55 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n).split('')
+    let number
+    if (str.includes('-')) {
+        number = str.slice(1).reverse().join('')
+    }
+    else {
+        number = +str.reverse().join('')
+    }
+    return number
 }
